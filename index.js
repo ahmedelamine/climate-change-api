b81c4bfbf89d8994f707a83ca9d70bdb4914527c
ahmedelamine <ahmedelamine5@gmail.com>
2021-12-28 13:17:10 +0100

    init commit

---
+++ index.js
@@ -0,0 +1,9 @@
+import express from "express";
+import axios from "axios";
+import { Cheerio } from "cheerio";
+
+const app = express;
+
+
+
+app.listen(8000, () => console.log("Server running in PORT 8000"))
