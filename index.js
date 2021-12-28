fbb511850a4cd89de19695350a1e9d6ad047492f
ahmedelamine <ahmedelamine5@gmail.com>
2021-12-28 13:22:26 +0100

    routing in express

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
+import { config } from "dotenv";
+config();
+
 import express from "express";
 import axios from "axios";
-import { Cheerio } from "cheerio";
+import cheerio from "cheerio";
 
-const app = express;
+const app = express();
 
+app.get("/", (req,res) => {
+    res.json({message: "welcome to my Climate Change news api"})
+})
 
-
-app.listen(8000, () => console.log("Server running in PORT 8000"))
+const PORT = process.env.PORT || 4000
+app.listen(PORT, () => console.log(`Server running in PORT ${PORT}`))
