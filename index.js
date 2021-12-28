7a26d1ad6f4ac030619013850dcda750b879bb72
ahmedelamine <ahmedelamine5@gmail.com>
2021-12-28 14:16:49 +0100

    getting multiple news sources

--- index.js
+++ index.js
@@ -7,16 +7,80 @@ import * as cheerio from "cheerio";
 
 const app = express();
 
+const newspapers = [
+  {
+    name: "cityam",
+    address:
+      "https://www.cityam.com/london-must-become-a-world-leader-on-climate-change-action/",
+    base: "",
+  },
+  {
+    name: "thetimes",
+    address: "https://www.thetimes.co.uk/environment/climate-change",
+    base: "",
+  },
+  {
+    name: "guardian",
+    address: "https://www.theguardian.com/environment/climate-crisis",
+    base: "",
+  },
+  {
+    name: "telegraph",
+    address: "https://www.telegraph.co.uk/climate-change",
+    base: "https://www.telegraph.co.uk",
+  },
+  {
+    name: "nyt",
+    address: "https://www.nytimes.com/international/section/climate",
+    base: "",
+  },
+  {
+    name: "latimes",
+    address: "https://www.latimes.com/environment",
+    base: "",
+  },
+  {
+    name: "smh",
+    address: "https://www.smh.com.au/environment/climate-change",
+    base: "https://www.smh.com.au",
+  },
+  {
+    name: "un",
+    address: "https://www.un.org/climatechange",
+    base: "",
+  },
+  {
+    name: "bbc",
+    address: "https://www.bbc.co.uk/news/science_and_environment",
+    base: "https://www.bbc.co.uk",
+  },
+  {
+    name: "es",
+    address: "https://www.standard.co.uk/topic/climate-change",
+    base: "https://www.standard.co.uk",
+  },
+  {
+    name: "sun",
+    address: "https://www.thesun.co.uk/topic/climate-change-environment/",
+    base: "",
+  },
+  {
+    name: "dm",
+    address:
+      "https://www.dailymail.co.uk/news/climate_change_global_warming/index.html",
+    base: "",
+  },
+  {
+    name: "nyp",
+    address: "https://nypost.com/tag/climate-change/",
+    base: "",
+  },
+];
+
 const articles = [];
 
-app.get("/", (req, res) => {
-  res.json({ message: "welcome to my Climate Change news api" });
-});
-
-app.get("/news", async (req, res) => {
-  const url = "https://www.theguardian.com/environment/climate-crisis";
-  try {
-    const response = await axios.get(url);
+newspapers.forEach(({address, name, base}) => {
+  axios.get(address).then((response) => {
     const html = response.data;
     const $ = cheerio.load(html);
 
@@ -24,14 +88,39 @@ app.get("/news", async (req, res) => {
       const title = $(this).text();
       const url = $(this).attr("href");
 
-      articles.push({ title, url });
+      articles.push({
+        title,
+        url: base + url,
+        source: name,
+      });
     });
+  });
+});
 
-    res.json(articles);
-  } catch (error) {
-    console.log(error);
-    res.status(400).json({ message: "Internal Server Error" });
-  }
+app.get("/", (req, res) => {
+  res.json({ message: "welcome to my Climate Change news api" });
+});
+
+app.get("/news", async (req, res) => {
+  //   const url = "https://www.theguardian.com/environment/climate-crisis";
+  //   try {
+  //     const response = await axios.get(url);
+  //     const html = response.data;
+  //     const $ = cheerio.load(html);
+
+  //     $('a:contains("climate")', html).each(function () {
+  //       const title = $(this).text();
+  //       const url = $(this).attr("href");
+
+  //       articles.push({ title, url });
+  //     });
+
+  //     res.json(articles);
+  //   } catch (error) {
+  //     console.log(error);
+  //     res.status(400).json({ message: "Internal Server Error" });
+  //   }
+  res.json(articles);
 });
 
 const PORT = process.env.PORT || 4000;
